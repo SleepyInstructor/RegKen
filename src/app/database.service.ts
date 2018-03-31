d7cb418722521346604602dbfaa516b4dcf1b15b
Kim Lam <kimlam@langara.ca>
2018-03-30 17:56:37 -0700

    Began management page

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DatabaseService {
 
+  DojoData: Array<any> = [{ Dojo: "A" },
+  { Dojo: "B" },
+  { Dojo: "C" },
+  { Dojo: "D" },
+  { Dojo: "E" },
+  { Dojo: "F" },
+  { Dojo: "G" }]
+
+  getTeams() {
+    return this.DojoData.map((value: any, index: number, array: any[]) => {
+      return value.Dojo;
+    });
+
+  }
   constructor() { }
 
 }
--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DatabaseService} from '../database.service';
 
 @Component({
   selector: 'app-management',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagementComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db : DatabaseService) { 
+    
+  }
 
   ngOnInit() {
   }

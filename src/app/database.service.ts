16105b76491864838b558307bdbafaac37f50631
Kim Lam <kimlam@langara.ca>
2018-04-02 09:34:55 -0700

    update

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -3,20 +3,23 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DatabaseService {
 
-  DojoData: Array<any> = [{ Dojo: "A" },
-  { Dojo: "B" },
-  { Dojo: "C" },
-  { Dojo: "D" },
-  { Dojo: "E" },
-  { Dojo: "F" },
-  { Dojo: "G" }]
+  DojoData: Array<any>;
 
   getTeams() {
     return this.DojoData.map((value: any, index: number, array: any[]) => {
+
       return value.Dojo;
     });
 
   }
-  constructor() { }
+  constructor() { 
+    this.DojoData = [{ Dojo: "A" },
+    { Dojo: "B" },
+    { Dojo: "C" },
+    { Dojo: "D" },
+    { Dojo: "E" },
+    { Dojo: "F" },
+    { Dojo: "G" }]
+  }
 
 }
